d164b316014b761428c5e963ae3e9acd759924a3
Rafael <82333433+rafaelrafaelfer@users.noreply.github.com>
2021-08-30 17:45:21 -0300

    Update MY.sql

--- MY.sql
+++ MY.sql
@@ -1,8 +1,18 @@
 CREATE TABLE pessoas(
     id INT NOT NULL PRIMARY KEY autoincrement, 
     nome VARCHAR (30) NOT NULL,
-    Nascimento DATE
+    nascimento DATE
 )    
-INSERT INTO pessoas(nome, nascimento) VALUES ('RAFAEL', '1990 08 31')
-INSERT INTO pessoas(nome, nascimento) VALUES ('PEDRO', '1990 07 12')
-INSERT INTO pessoas(nome, nascimento) VALUES ('MARCELA', '1990 04 11')
+INSERT INTO pessoas(nome, nascimento) VALUES ('RAFAEL', '1990 08 31');
+INSERT INTO pessoas(nome, nascimento) VALUES ('PEDRO', '1990 07 12');
+INSERT INTO pessoas(nome, nascimento) VALUES ('MARCELA', '1990 04 11');
+
+UPDATE pessoas SET nome = 'Rafael Ferreira' WHERE id = '1'
+
+DELETE FROM pessoas WHERE id = 4;
+
+SELECT FROM pessoas ORDER BY nome;
+SELECT FROM pessoas ORDER BY DESC;
+
+
+
